cd03125cd30d8b0617cb1e4f9b12cd1b9830f95c
Luke.Batchelor <batchelor.luke+git@gmail.com>
2021-03-07 19:48:58 +1100

    Removes last parts of shared

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import React, { useContext, useEffect } from 'react';
 import { CircularProgress, createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core';
 import { navigate, Router, useLocation, useMatch } from '@reach/router';
-import { useQuery, useQueryClient, QueryClient, QueryClientProvider } from 'react-query';
-import { UserResponse } from '@repo/shared/User';
 
 import { AdminUI } from './pages/AdminUI';
 import { Login } from './pages/Login';
 import { Home } from './pages/Home';
 import { SignUp } from './pages/SignUp';
-import { useMeQuery } from './api';
 
 const theme = responsiveFontSizes(createMuiTheme());
 
